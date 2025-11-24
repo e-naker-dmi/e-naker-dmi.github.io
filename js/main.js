01dd6a5741467065723e6e2e701ffcecef96b155
dumaikota <nakerdinas1234@gmail.com>
2025-11-24 20:48:57 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Fungsi untuk mengatur efek splash pada halaman
+window.onload = function() {
+  setTimeout(function() {
+    document.getElementById('splash').classList.add('fade');
+  }, 300); // Durasi 0.3 detik
+};
