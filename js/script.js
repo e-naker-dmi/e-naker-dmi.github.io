90d03e47348ea8017d133c470331800ca32293ac
dumaikota <nakerdinas1234@gmail.com>
2025-11-24 23:50:55 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js - Fungsi global yang digunakan di seluruh aplikasi
+
+// Misalnya untuk navigasi atau manipulasi DOM umum
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Aplikasi e-NAKER-DMI siap!");
+});
