cd987a571f2d9c54ee9bc0394ae997e044073cc0
dumaikota <nakerdinas1234@gmail.com>
2025-11-25 04:38:00 +0700

    Create script.js

---
+++ modules/lowongan-kerja/script.js
@@ -0,0 +1,7 @@
+// Menambahkan event listener untuk form lowongan
+document.getElementById("form-lowongan").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Lowongan berhasil ditambahkan!");
+});
+
+// Menambahkan fungsi lainnya jika diperlukan
